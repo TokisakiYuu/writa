eb0937eebc8921e1147b74357e4f6734150d55b2
zhaoliping <tokisakiyuu@qq.com>
2020-11-04 22:48:47 +0800

    打印日志

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -2,6 +2,10 @@ import Koa from "koa";
 
 export default function() {
     return async (ctx:Koa.ParameterizedContext, next:Koa.Next) => {
-        return next();
+        let { method, url } = ctx;
+        let startTime = Date.now();
+        await next();
+        let endTime = Date.now();
+        console.log(`[${method.toUpperCase()}] ${url} ${(endTime - startTime) / 1000}ms`);
     }
 }
