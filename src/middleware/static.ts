ede8b20215e7b3a7eee154acd6ccc80437e96fab
tokisakiyuu <tokisakiyuu@qq.com>
2020-11-04 16:39:24 +0800

    脚手架

--- src/middleware/static.ts
+++ src/middleware/static.ts
@@ -4,10 +4,11 @@ import mount from "koa-mount";
 import serve from "koa-static";
 import { assetsConfig } from "../../frontend";
 
+const PUBLIC_ROOT = "/static"
 let staticPath = resolve(assetsConfig.distDir);
 
 export default function() {
   const resourceApp = new koa();
   resourceApp.use(serve(staticPath));
-  return mount('/static', resourceApp);
+  return mount(PUBLIC_ROOT, resourceApp);
 }
