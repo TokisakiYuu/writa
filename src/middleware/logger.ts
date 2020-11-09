40a717a002e33b6d1e421e710598ecd3a71c2699
zhaoliping <tokisakiyuu@qq.com>
2020-11-09 23:59:24 +0800

    refactor(log): no longer count cost time

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -3,9 +3,7 @@ import Koa from "koa";
 export default function() {
     return async (ctx:Koa.ParameterizedContext, next:Koa.Next) => {
         let { method, url } = ctx;
-        let startTime = Date.now();
         await next();
-        let endTime = Date.now();
-        console.log(`[${method.toUpperCase()}] ${ctx.status} ${url} ${endTime - startTime}ms`);
+        console.log(`[${method.toUpperCase()}] ${ctx.status} ${url}`);
     }
 }
