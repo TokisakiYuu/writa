3155ffa94ee872eea3fbd2011663a0bd5fe5f233
tokisakiyuu <tokisakiyuu@qq.com>
2020-11-12 14:18:28 +0800

    fix: middleware methods(eg. `ctx.sendHTML`) declarations merge into koa declaration
    this make us to get the code hints provided by IDE

--- src/routes/home/index.ts
+++ src/routes/home/index.ts
@@ -10,7 +10,7 @@ const router = new Router();
 router
     .get("/", async (ctx:Koa.Context, next:Koa.Next) => {
         ctx.push("/css/shell.css").to("/static/css/shell.css");
-        await ctx.sendHTML("index", {author: "tokisakiyuu", email: "tokisakiyuu@qq.com"});
+        ctx.sendHTML("index", {author: "tokisakiyuu", email: "tokisakiyuu@qq.com"});
         return next();
     })
 export default router.routes();
