aef2fdbcc17bde95dd6906fc410df6d0e27440e2
tokisakiyuu <tokisakiyuu@qq.com>
2021-01-14 15:24:59 +0800

    refactor(spa): .tsx change to .ts

--- src/controllers/spa.ts
+++ src/controllers/spa.ts
@@ -1,4 +1,5 @@
-import { Context, Next } from "koa";
+import { Next } from "koa";
+import { RouterContext } from "koa-router";
 import React from "react";
 import App from "../public/components/App";
 import render from "../util/htmlRender";
@@ -9,11 +10,10 @@ import render from "../util/htmlRender";
  * @description
  * 单页应用，根据url来渲染首屏页面的html
  */
-export const SPA = async (ctx: Context, next: Next) => {
+export const SPA = async (ctx: RouterContext, next: Next) => {
   if(ctx.method.toUpperCase() !== "GET") return next();
   if(!(ctx.headers.accept as string).startsWith("text/html")) return next();
-  await next();
   const data = { article: { title: "hello" , content: "花菜" } };
   ctx.type = "html";
-  ctx.body = render(<App data={data} />);
+  ctx.body = render(React.createElement(App, { data }));
 };
