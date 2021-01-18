90a700e74f50d7e2cc0def361bc1e1361d9b75db
zhaoliping <tokisakiyuu@qq.com>
2021-01-18 22:40:38 +0800

    feat: build page styles use styled-components

--- src/public/components/App.tsx
+++ src/public/components/App.tsx
@@ -2,17 +2,31 @@ import React, {
   FC
 } from "react";
 import Button, { PrimaryButton } from "./Button";
+import styled from "styled-components";
 
 const App: AppComponent = ({ data }) => {
-  const dataJSON = JSON.stringify(data);
   return (
     <>
-      <script type={"text/javascript"} dangerouslySetInnerHTML={{__html: `var _DATA_ = ${dataJSON}`}}></script>
       <Button alertText={"标题: " + data.article.title} />
       <PrimaryButton alertText={"内容: " + data.article.content} content="问" />
+      <Div>
+        <p>
+          你好我是有鱼
+          <span>哈哈哈</span>
+        </p>
+      </Div>
     </>
   );
 };
+
+const Div = styled.div`
+  & > p {
+    color: green;
+    span {
+      color: red;
+    }
+  }
+`;
 
 type AppComponent = FC<{
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
--- src/public/components/Button.tsx
+++ src/public/components/Button.tsx
@@ -1,9 +1,15 @@
 import React, {
   FC
 } from "react";
+import styled from "styled-components";
+
+const MButton = styled.button`
+  color: blue;
+  font-size: 50px;
+`;
 
 const Button: DefaultButton = ({ alertText }) => (
-  <button onClick={() => alert(alertText)}>提示</button>
+  <MButton onClick={() => alert(alertText)}>提示</MButton>
 );
 
 const PButton: PrimaryButton = ({ alertText, content }) => (
@@ -12,6 +18,7 @@ const PButton: PrimaryButton = ({ alertText, content }) => (
 
 type DefaultButton = FC<{
   alertText?: string;
+  textColor?: string;
 }>
 
 type PrimaryButton = FC<{
