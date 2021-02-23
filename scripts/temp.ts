5adca66e6b24cac4de7b6a20aa86ce2be774a5cc
tokisakiyuu <tokisakiyuu@qq.com>
2021-02-23 17:55:01 +0800

    chore: clean

--- scripts/temp.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import { MONGODB_URI } from "../src/util/secrets";
-import ArticleModel, { ArticleDocument } from "../src/models/Article";
-
-// (async () => {
-//   await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-//   const doc: ArticleDocument = await ArticleModel.findOne({ title: "Do you like your room ?" });
-//   console.log(doc);
-// })();
