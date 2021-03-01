a1efdf84b7ade07735cc2119858a5f2284d2e165
tokisakiyuu <tokisakiyuu@qq.com>
2021-03-01 18:15:39 +0800

    build: partial packages replace with scoped packages

--- babel.config.js
+++ babel.config.js
@@ -8,8 +8,8 @@ module.exports = {
   ],
   plugins: [
     "@babel/plugin-transform-runtime",
+    "@babel/plugin-transform-react-display-name",
+    "@babel/plugin-proposal-class-properties",
     "babel-plugin-macros",
-    "transform-class-properties",
-    "babel-plugin-add-react-displayname"
   ]
 };
