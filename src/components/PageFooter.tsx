01d52d1291989e4a42848e54a8784d780f0af4a6
tokisakiyuu <tokisakiyuu@qq.com>
2021-03-09 12:33:00 +0800

    feat: use reactive.macro in base components

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,11 +1,16 @@
 import React, { FC } from "react";
 import styled from "styled-components/macro";
+import { state, bind } from "reactive.macro";
 
-const PageFooter: FC<{}> = ({ children }) => (
-  <Box>
-    <span>我是page footer</span>
-  </Box>
-);
+const PageFooter: FC<{}> = () => {
+  const num = state(1);
+  return (
+    <Box>
+      <input type="number" value={bind(num)} />
+      <span>我是page footer {num}</span>
+    </Box>
+  );
+};
 
 const Box = styled.footer`
   background-color: blue;
