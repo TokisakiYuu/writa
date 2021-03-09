ccc76dd5feb41d622013daf9fef1ee93535849a2
tokisakiyuu <tokisakiyuu@qq.com>
2021-03-09 17:28:16 +0800

    refactor: print graphql schema

--- scripts/print-graphql-schema.ts
+++ scripts/print-graphql-schema.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { printSchema } from "graphql";
-import schema from "../src/data/types/Schema";
+import { schema } from "../src/data/schema";
 
 const code = printSchema(schema, { commentDescriptions: true });
 const filePath = path.resolve(__dirname, "../assets/schema.graphql");
